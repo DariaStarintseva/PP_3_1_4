6d6d2c7c10c01298ccfd9be2687891c089d2960c
DariaStarintseva <yudina.daria@inbox.ru>
2023-09-18 13:15:56 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -0,0 +1,63 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping()
+    public String userList(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        model.addAttribute("user", userService.loadUserByUsername(username));
+        model.addAttribute("admin", userService.allUsers());
+        return "admin";
+    }
+
+    @GetMapping("/registration")
+    public String addNewUserPage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        model.addAttribute("user", userService.loadUserByUsername(username));
+        List<Role> roles = roleService.getAllRoles();
+        model.addAttribute("roles", roles);
+        return "new";
+    }
+
+    @PostMapping("/registration")
+    public String addNewUser(@ModelAttribute("user") User user) {
+        userService.register(user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}")
+    public String  delete(@PathVariable("id") long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") User user, @PathVariable("id") long id) {
+        userService.updateUser(id,user);
+        return "redirect:/admin";
+    }
+}
